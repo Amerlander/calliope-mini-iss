947ff90adbf1d45d65d7e0a021b87fc5151eeca8
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 00:13:28 +0200

    add enums & shims

--- shims.d.ts
+++ shims.d.ts
@@ -1,30 +1,27 @@
+// Auto-generated. Do not edit.
 
-#include "pxt.h"
-using namespace pxt;
 
-/**
- * 
- */
-namespace input {
+    /**
+     * 
+     */
+
+declare namespace input {
 
     /**
      * Obsolete, compass calibration is automatic.
      */
     //% help=input/calibrate-compass advanced=true
     //% blockId="input_compass_is_calibrated" block="is compass calibrated?"
-    //% weight=45
-    int isCalibratedCompass() {
-        return uBit.compass.isCalibrated();
-    }
+    //% weight=45 shim=input::isCalibratedCompass
+    function isCalibratedCompass(): int32;
 
     /**
      * Obsolete, compass calibration is automatic.
      */
     //% help=input/calibrate-compass advanced=true
     //% blockId="input_compass_clear_calibration" block="clear compass calibration"
-    //% weight=45
-    void clearCalibrationCompass() {
-        uBit.compass.clearCalibration();
-    }
-
+    //% weight=45 shim=input::clearCalibrationCompass
+    function clearCalibrationCompass(): void;
 }
+
+// Auto-generated. Do not edit. Really.
