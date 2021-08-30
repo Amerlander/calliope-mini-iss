709337947d22408ec8634341319ef1d519758396
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 05:07:06 +0200

    test12

--- compassDriver.cpp
+++ compassDriver.cpp
@@ -1,6 +1,21 @@
 #include "compassDriver.h"
 
-MicroBitCompassDriver::MicroBitCompassDriver();
+/**
+ * Constructor.
+ * Create a software abstraction of an e-compass.
+ *
+ * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_COMPASS
+ * @param accel the accelerometer to use for tilt compensation
+ * @param coordinateSpace the orientation of the sensor. Defaults to: SIMPLE_CARTESIAN
+ *
+ */
+MicroBitCompassDriver::MicroBitCompassDriver(MicroBitAccelerometer &accel, CoordinateSpace &cspace, uint16_t id) :  calibration(), sample(), sampleENU(), coordinateSpace(cspace)
+{
+    accelerometer = &accel;
+    // Store our identifiers.
+    this->id = id;
+    this->status = 0;
+}
 
 /**
  * Assume the Compass has callibration data and set it as callibrated
--- compassDriver.h
+++ compassDriver.h
@@ -5,7 +5,16 @@ class MicroBitCompassDriver : public MicroBitCompass
     
     public:
 
-        MicroBitCompassDriver();
+        /**
+         * Constructor.
+         * Create a software abstraction of an e-compass.
+         *
+         * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_COMPASS
+         * @param coordinateSpace the orientation of the sensor. Defaults to: SIMPLE_CARTESIAN
+         *
+         */
+        MicroBitCompassDriver(CoordinateSpace &coordinateSpace, uint16_t id = MICROBIT_ID_COMPASS);
+
 
         void assumeCalibrated();
 
