e4dc4a0dc6a2d5ed9d794956f48d283010333278
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 14:42:29 +0200

    Aktualisiere compassDriver.h, compass.cpp, compassDriver.cpp

--- compass.cpp
+++ compass.cpp
@@ -34,8 +34,8 @@ namespace input {
     //% blockId="input_compass_assume_calibration" block="assume compass calibrated"
     //% weight=45
     void assumeCalibratedCompass() {
-        MicroBitCompassDriver cd;
-        cd.assumeCalibrated();
+        // MicroBitCompassPre mbcp;
+        // mbcp.isCalibrated();
     }
 
 }
