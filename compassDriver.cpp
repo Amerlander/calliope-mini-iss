128415beaf114b02073718c01bb3b4424e6c7c81
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 04:38:43 +0200

    test8

--- compassDriver.cpp
+++ compassDriver.cpp
@@ -1,5 +1,11 @@
 #include "compassDriver.h"
 
+
+/**
+ * Constructor.
+ */
+MicroBitCompassDriver::MicroBitCompassDriver() { }
+
 /**
  * Assume the Compass has callibration data and set it as callibrated
  *
