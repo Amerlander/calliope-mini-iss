8892293465dee58e44ec565753673f7f643e854b
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 03:13:27 +0200

    test

--- compassDriver.cpp
+++ compassDriver.cpp
@@ -1,11 +1,13 @@
+#include "pxtcore.h"
 // #include "MicroBitCompass.h"
+using namespace MicroBit;
 
-// /**
-//  * Assume the Compass has callibration data and set it as callibrated
-//  *
-//  */
-// void MicroBitCompass::assumeCalibrated()
-// {
-//     status |= MICROBIT_COMPASS_STATUS_CALIBRATED;
-//     // status |= 0x02;
-// }
+/**
+ * Assume the Compass has callibration data and set it as callibrated
+ *
+ */
+void assumeCalibrated()
+{
+    // status |= MICROBIT_COMPASS_STATUS_CALIBRATED;
+    status |= 0x02;
+}
--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     input.clearCalibrationCompass()
 })
+input.assumeCalibratedCompass()
 basic.forever(function () {
     basic.showNumber(input.isCalibratedCompass())
 })
