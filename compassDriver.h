b04517f54b77e3aa7c8dea6d58d4430a185524f9
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 05:12:14 +0200

    test13

--- compassDriver.h
+++ compassDriver.h
@@ -7,14 +7,9 @@ class MicroBitCompassDriver : public MicroBitCompass
 
         /**
          * Constructor.
-         * Create a software abstraction of an e-compass.
-         *
-         * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_COMPASS
-         * @param coordinateSpace the orientation of the sensor. Defaults to: SIMPLE_CARTESIAN
          *
          */
-        MicroBitCompassDriver(CoordinateSpace &coordinateSpace, uint16_t id = MICROBIT_ID_COMPASS);
-
+        MicroBitCompassDriver();
 
         void assumeCalibrated();
 
