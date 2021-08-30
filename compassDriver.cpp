c8cac6020edeb217c5037e73a45f8d029713c535
Amerlander <Amerlander@users.noreply.github.com>
2021-08-30 14:45:40 +0200

    fix include

--- compassDriver.cpp
+++ compassDriver.cpp
@@ -22,7 +22,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-#include "MicroBitCompassPre.h"
+#include "compassDriver.h"
 #include "ErrorNo.h"
 #include "MicroBitEvent.h"
 #include "MicroBitCompat.h"
