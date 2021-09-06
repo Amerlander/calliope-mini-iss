82b5e1860f4d0b6a75e3cb847859747a5d21ee29
Amerlander <Amerlander@users.noreply.github.com>
2021-09-07 01:06:01 +0200

    Aktualisiere enums.d.ts, BMX055MagnetometerDriver.h, BMX055MagnetometerDriver.cpp

--- enums.d.ts
+++ enums.d.ts
@@ -1,5 +1,29 @@
 // Auto-generated. Do not edit.
+
+
+    /**
+     * Term to convert sample data into SI units.
+     */
+
+    declare const enum BMX055_M_MODE {
+    lowPower = 0,
+    regular = 1,
+    enhancedRegular = 2,
+    highAccuracy = 3,
+    }
 declare namespace input {
 }
+
+
+    declare const enum MODR {
+    MODR_10Hz = 0,
+    MODR_2Hz = 1,
+    MODR_6Hz = 2,
+    MODR_8Hz = 3,
+    MODR_15Hz = 4,
+    MODR_20Hz = 5,
+    MODR_25Hz = 6,
+    MODR_30Hz = 7,
+    }
 
 // Auto-generated. Do not edit. Really.
